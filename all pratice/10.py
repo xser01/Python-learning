3731994b6ab9763bae50389a4e1c3d2058945e6f
xser01 <notlate_09@outlook.com>
2026-08-12 06:05:52 +0000

    dict

---
+++ all pratice/10.py
@@ -0,0 +1,11 @@
+def sort_dict(d):
+    list_tuple=[]
+    for key,value in d.items():
+        list_tuple.append((key,value))
+    return sorted(list_tuple,key=lambda x:x[1],reverse=True)
+def sort_dict(d):
+    return sorted(d.items(),key=lambda x:x[1],reverse=True)
+#对于key函数，key＝函数，key后必须接函数
+#lambda函数，格式为lambda x：x【0】，第一个x为接收进去的数据，x【0】为实际从lambda函数输出到外部的内容
+#dict数据类型中的items（）方法，返回的是一个【（a：b），（c：s）】的东西，本质还是dict类型
+#需要将其转为list类型才能使用list方法，但items后可以游历操作的，因为它本质还是一个iterable，可迭代对象。
