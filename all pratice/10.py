94f5947773a83e38f0bfd9098627b3cfe5524f13
xser01 <notlate_09@outlook.com>
2026-08-12 06:08:41 +0000

    dict2

--- all pratice/10.py
+++ all pratice/10.py
@@ -9,3 +9,10 @@ def sort_dict(d):
 #lambda函数，格式为lambda x：x【0】，第一个x为接收进去的数据，x【0】为实际从lambda函数输出到外部的内容
 #dict数据类型中的items（）方法，返回的是一个【（a：b），（c：s）】的东西，本质还是dict类型
 #需要将其转为list类型才能使用list方法，但items后可以游历操作的，因为它本质还是一个iterable，可迭代对象。
+'''
+dict_items([
+    ("a", 3),
+    ("b", 1),
+    ("c", 5)
+])
+'''#对于items，得到的是一个dict_items对象，字典视图对象，如上
